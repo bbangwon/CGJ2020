844f4eecc420e7949f089c64c859e6d8e769520e
DESKTOP-6C85CV4\bbang <bbangwon.chung@gmail.com>
2020-12-05 00:52:48 +0900

    Joycon Lib Add

---
+++ Assets/0.KoZae/Scripts/0.Library/Tools/Singleton/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Singleton<T> where T : class
+{
+    private static readonly object sync = new object();
+    private static volatile T instance = null;
+
+    public static T In
+    {
+        get
+        {
+            if (instance == null)
+            {
+                CreateInstance();
+            }
+
+            return instance;
+        }
+    }
+    
+    static void CreateInstance()
+    {
+        lock (sync)
+        {
+            if (instance == null)
+            {
+                instance = (T)Activator.CreateInstance(typeof(T),true);
+            }
+        }
+    }
+
+    public virtual void Destroy() 
+    {
+        if (instance != null)
+        {
+            instance = null;
+        }
+    }
+
+    public static bool HasInstance => instance != null;
+}
---
+++ Assets/0.KoZae/Scripts/0.Library/Tools/Singleton/SingletonMono.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class SingletonMono<T> : MonoBehaviour where T : Component
+{
+    protected static T instance = null;
+    public static T In
+    {
+        get
+        {
+            if(applicationIsQuitting)
+            {
+                return null;
+            }
+            else
+            {
+                if(instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+
+                    if(instance == null)
+                    {
+                        instance = new GameObject(typeof(T).Name).AddComponent<T>();
+
+                        DontDestroyOnLoad(instance);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if(instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DoAwake();
+    }
+
+    public static bool HasInstance => !IsDestroyed;
+    public static bool IsDestroyed => instance == null;
+
+    protected virtual void DoAwake() { }
+
+    protected static bool applicationIsQuitting = false;
+    protected virtual void OnApplicationQuit() { applicationIsQuitting = true; }
+}
